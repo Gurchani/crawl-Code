ac145d0718d3bfeff4648e18ebcb18af3313aa26
Gurchani101 <umergurchani5@gmail.com>
2021-04-04 17:22:14 +0200

    processRetweeters.py create table if not exits does not create a new table

--- processRetweeters.py
+++ processRetweeters.py
@@ -2,6 +2,8 @@
 def getHighFrequencyRetweeters(leaderName , database):
     Query = 'Create table IF NOT EXISTS '+leaderName+'retweeterFreq as Select retweeterId, count(*) as freq from ' + leaderName+'retweeters ' \
                                                                       'group by retweeterId order by freq desc'
+
+    print(Query)
     database.execute(Query)
 
 def process(leaderNames, database):
